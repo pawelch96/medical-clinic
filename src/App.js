65195eadf0ec3ae73d81a4fd385ef1e6ef932985
pawelch96 <pawelchmiel.20.03@gmail.com>
2019-12-29 21:55:28 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import HomePage from "./pages/homepage";
+import StaffPage from "./pages/staffpage";
+import ContactPage from "./pages/contactpage";
+import Header from "./components/header/header.component";
+import ErrorBoundary from "./components/error-boundary/error-boundary.component";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/staff" component={StaffPage} />
+          <Route exact path="/contact" component={ContactPage} />
+          <Route component={ErrorBoundary} />
+        </Switch>
+      </Router>
+    </React.Fragment>
   );
 }
 
